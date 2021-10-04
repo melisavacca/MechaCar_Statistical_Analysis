5ec6611ca8759a95b486e3a154e93d1ad34e1f77
Melisa Vacca <melisa.vacca@gmail.com>
2021-10-04 12:54:39 -0400

    adding deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,3 +6,11 @@ MechaCar <- read.csv(file="MechaCar_mpg.csv")
 # Perform linear regression
 reg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar)
 summary(reg)
+
+suspension <- read.csv(file="Suspension_Coil.csv")
+
+total_summary <- summarize(suspension, mean(PSI), median(PSI), var(PSI), sd(PSI), .groups= 'keep')
+total_summary
+
+lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(mean(PSI), median(PSI), var(PSI), sd(PSI), .groups= 'keep')
+lot_summary
