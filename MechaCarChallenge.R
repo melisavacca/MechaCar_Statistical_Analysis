24addc06b9ac43757497fdf9858f1d9bae76a86f
Melisa Vacca <melisa.vacca@gmail.com>
2021-10-04 14:44:16 -0400

    adding deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,10 +3,11 @@ library(dplyr)
 
 MechaCar <- read.csv(file="MechaCar_mpg.csv")
 
-# Perform linear regression
+# Deliverable 1
 reg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar)
 summary(reg)
 
+#Deliverable 2
 suspension <- read.csv(file="Suspension_Coil.csv")
 
 total_summary <- summarize(suspension, mean(PSI), median(PSI), var(PSI), sd(PSI), .groups= 'keep')
@@ -14,3 +15,9 @@ total_summary
 
 lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(mean(PSI), median(PSI), var(PSI), sd(PSI), .groups= 'keep')
 lot_summary
+
+#Deliverable 3
+t.test(suspension$PSI,mu=1500)
+t.test(subset(suspension$PSI,suspension$Manufacturing_Lot=='Lot1'),mu=1500)
+t.test(subset(suspension$PSI,suspension$Manufacturing_Lot=='Lot2'),mu=1500)
+t.test(subset(suspension$PSI,suspension$Manufacturing_Lot=='Lot3'),mu=1500)
